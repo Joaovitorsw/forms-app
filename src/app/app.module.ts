91cfcb7e5d49ae20aa52bfdb426e419609f32d0f
João Vitor Pereira dos Santos <joaovitorswbr@gmail.com>
2021-12-27 18:34:18 -0300

    ✨ feat: mat-icon and mat-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreviewCardComponent } from './components/preview-card/preview-card.component';
 import { HomePage } from './pages/home/home.page';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePage],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomePage, PreviewCardComponent, SearchComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatIconModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
